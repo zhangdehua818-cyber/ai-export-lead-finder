6dc0ab03194ba1e88cdddedfefd36ee87e3d3f0f
zhangdehua818-cyber <zhangdehua818@gmail.com>
2026-09-01 21:53:11 +0800

    Create websiteParser.js

---
+++ modules/websiteParser.js
@@ -0,0 +1,33 @@
+export async function parseWebsite(url){
+
+
+return {
+
+
+company:
+
+"未知公司",
+
+
+industry:
+
+"未知行业",
+
+
+emails:[
+
+"待发现"
+
+],
+
+
+description:
+
+"网站信息分析"
+
+
+
+};
+
+
+}
